251bde0dd7836fca90628009d6e825512878c4b0
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-11-05 22:00:38 +0300

    :art: added css for the invest section

--- src/Components/Invest.tsx
+++ src/Components/Invest.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import './Invest.css'
 
 function Invest() {
   return (
@@ -8,35 +8,33 @@ function Invest() {
 <img className='background-image' src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1679982347/elewa-group-website/hero-Images/dark-to-light_xrat5g.jpg'></img>
   </div>
 
-  <div className='logo'>
- <img
+    
+  <div className='header2'>
+        <div className='nav-header light-border'>
+          <img
             className='logo'
             src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg'
             alt='logo'
           ></img>
-  </div>
-  <div className='navlinks'>
-           <ul>
-        <li>
-          <Link to="/">Invest</Link>
-        </li>
-
-        <li>
-          <Link to="/contact">SocialImpact</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </div>
-
-
-
-      <div className='investing'>
-        <h2>Trade, not aid</h2>
-        <h1>Southern innovations addressing global problems</h1>
+          <div>
+        {/* this is the menu and some items for the navbar */}
+            <ul className='nav-items'>
+              <li><a href='/about'>About us</a></li>
+              <li><a href='/'>Social Impact</a></li>
+              <li><a href='/invest'>Invest</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
 
+<div className='outer-investing'>
+
+        <div className='inner-investing'>
+        <h2>Trade, not aid</h2>
+        <br/>
+        <h1>Southern innovations addressing <br/>global problems</h1>
+      </div>
+</div>
       <div className='invest-section'>
         <div className='second-invest-section'>
           <div className='invest-info'>
@@ -46,85 +44,99 @@ function Invest() {
           <div className='invest-image'>
             <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png' alt='image'></img>
           </div>
-        </div>
+        
+     
       </div>
-<div className='container2'>
-<h1>How we see it differently</h1>
-<p>The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development, innovation and drive going on in this part of the world (East-Africa, South Asia and beyond).
+  
 
-</p>
-<div className='container2-image'>
-  <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,w_1280/v1675690306/elewa-group-website/Images/Mask_Group_17_rjkgrq.png' alt='my-image'></img>
-</div>
+
+
 <div className='keyFigures'>
-  <h1>Key figures</h1>
-<div className='figure-container1'>
-  <h1>1.3M</h1>
+  <div className='inner-keyFigures'>
+    <div className='key'>
+ <h1>Key figures</h1>
+    </div>
+ <div className='keyFig-main'>
+<div className='figure-container'>
+  <h1>1.3<span className='figure-small'>M</span></h1>
   <p>Approx. number of Kenyan talents reaching the age of 18 in 2022</p>
 </div>
-<div className='figure-container2'>
-  <h1>5M</h1>
+<div className='figure-container'>
+  <h1>5<span className='figure-small'>M</span></h1>
   <p>number of East-Africans(EAC) reaching the age of 18 in 2022</p>
 </div>
-<div className='figure-container3'>
+<div className='figure-container'>
   <h1>2022</h1>
   <p>Democratic Republic of Congo joins the East African Community</p>
 </div>
-<div className='figure-container4'>
-  <h1>1.5M</h1>
+<div className='figure-container'>
+  <h1>1.5<span className='figure-small'>M</span></h1>
   <p>The number of unfilled IT vacancies in the US and EU markets</p>
    </div>
 
-  <div className='figure-container5'>
-<h1>10M
+  <div className='figure-container'>
+<h1>10<span className='figure-small'>M</span>
 </h1>
 <p>Number of technical vacancies by 2030</p>
   </div>
-  <div className='figure-container6'>
+  <div className='figure-container'>
     <h1>2050</h1>
     <p>Africa holds the worlds largest workforce</p>
   </div>
-  <div className='figure-container7'>
-    <h1>$4.6M</h1>
+  <div className='figure-container'>
+    <h1>$4.6<span className='figure-small'>M</span></h1>
     <p>Startup funding raised by African startups in 2022</p>
   </div>
 
-  <div className='figure-container7'>
-    <h1>310%</h1>
+  <div className='figure-container'>
+    <h1>310<span className='figure-small'>%</span></h1>
     <p>Increase in startup funding for companies with HQ in Kenya</p>
   </div>
+ </div>
 
+</div>
 </div>
 
 <div className='elewa-section'>
-  <div className='elewa-content'>
-    <h1>Elewa NV, a multi-stakeholder cooperation</h1>
-    <p>At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.
-
-Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.</p>
-  </div>
-  <div className='elewa-content-image'>
+  <div className='elewa-section-inner'>
+         <div className='elewa-content-image'>
 <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690303/elewa-group-website/Images/IMG_6523_vabpyo.png' alt='image'></img>
   </div>
+  <div className='elewa-content'>
+    <h1>Elewa NV, a multi-stakeholder cooperation</h1>
+    <p>At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.<br/></p>
+     <p>Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.</p>
+  </div>
+  </div>
+   
+
 </div>
 </div>
 
-<div className='creative-hub'></div>
-<div className='creative-hub-content'>
+<div className='creative-hub'>
+  <div className='inner-creative-hub'>
+    <div className='creative-hub-content'>
 <h1>The Elewa Creative Hub</h1>
-<p className='creative-hub-content-paragraph'>Coming soon; The Elewa Creative Hub Lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's Activities under a single banner.
-
-Through partnerships with creative community, the Elewa Hub will organize vibrant activites that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovation.</p>
+<p className='creative-hub-content-paragraph'>Coming soon; The Elewa Creative Hub Lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's Activities under a single banner.</p>
+<p>Through partnerships with creative community, the Elewa Hub will organize vibrant activites that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovation.</p>
 </div>
 <div className='creative-hub-content-image'>
   <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1679983012/elewa-group-website/Images/creative-hub-image_dd6o8o.jpg' alt='image'></img>
 </div>
 
-<div className='investing-section'>
-<div className='investing-section-content'>
+  </div>
+</div>
+
+<div className='elewa-section'>
+<div className='elewa-section-inner'>
+  <div className='elewa-content-image2'>
+    <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1679983082/elewa-group-website/Images/invest-now_ftxg4z.jpg'></img>
+    </div>
+    <div className='elewa-content'>
 <h1>Investing in Elewa NV</h1>
 <p>From time to time, we offer investment opportunities (private equity & debt) to out community of investors. Interested to take part when this opportunity arrives?</p>
 <button>Join the waitlist</button>
+</div>
 </div>
 </div>
 <div className='part-of'>
@@ -132,19 +144,18 @@ Through partnerships with creative community, the Elewa Hub will organize vibran
 And start investing</h2>
 </div>
 
-   <div className='footer'>
-        <div className='footer-container'>
-            <div className='footer-container-1'>
-                <div className='Headquaters'>
+     <div className='footer'>
+        <div className='footer-inner'>
+            <div className='footer-contacts'>
+                <div className='Headquartes footer-divs'>
                     <h2>Headquarters</h2>
                     <p>The Promenade, 19 General <br></br> Mathenge Rd <br></br> Nairobi, Kenya</p>
                 </div>
-                <div className='contacts-list'>
+                <div className='contacts footer-divs'>
                     <h2>Contacts</h2>
-                    
                     <p>T +254 78 92 27 755 <br></br> E info@elewa.ke</p>
                 </div>
-                <div className='navigation'>
+                <div className='navigation footer-divs'>
                     <h2>Navigation</h2>
                     <p>About us</p>
                     <p>Social<br></br>impact</p>
@@ -152,33 +163,34 @@ And start investing</h2>
                     <p>News</p>
                     <p>Contact</p>
                 </div>
-                <div  className='brands'>
+                <div  className='brands footer-divs'>
                     <h2>Brands</h2>
                     <p>Elewa</p>
                     <p>Italata</p>
                     <p>Venture labs</p>
                 </div>
-                <div className='privacy'>
+                <div className='privacy footer divs'>
                     <h2>Privacy</h2>
                     <p>Terms and<br></br>conditions</p>
                     <p>Cookie preferences</p>
                 </div>
             </div>
-            <div className='socials-tags'>
-                <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg' alt='image1'></img>
+            {/* social media platforms for the about page */}
+            <div className='footer-logo'>
+                <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg'></img>
                 <div className='social-media-icons'>
                     <div>
-                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/facebook_lhhwk2.svg' alt='image2'></img>
+                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/facebook_lhhwk2.svg'></img>
                     </div>
                     <div>
-                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg' alt='image3'></img>
+                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg'></img>
                     </div>
                     <div>
-                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/linkedin_x8smhm.svg ' alt='image3'></img>
+                        <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/linkedin_x8smhm.svg'></img>
                     </div>
                 </div>
             </div>
-                    </div>
+        </div>
       </div>
     </div>
   )

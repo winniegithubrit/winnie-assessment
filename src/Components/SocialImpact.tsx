03a2543379a6bfdc0b41a75d3194873d75e3b421
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-11-05 20:12:42 +0300

    :art: styled social impact component

--- src/Components/SocialImpact.tsx
+++ src/Components/SocialImpact.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './SocialImpact.css'
 function SocialImpact() {
   return (<div className='socialImpact'>
+
   <div className='heading'>
+    
 
     <div><img className='background-image' src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1679910469/elewa-group-website/hero-Images/pexels-bg-edit_bb4znx.jpg' alt='background'></img></div>
   <div className='header2'>
@@ -23,11 +25,37 @@ function SocialImpact() {
         </div>
       </div>
       <div className='heading-content'>
-        <h1>Scaling impact. Beyond the norm</h1>
-        <p>Human and Environmental Impact through Social Enterprise</p>
+        <div className='inner-heading-content'> 
+        <p>Scaling impact. Beyond the norm</p>
+        <h1>Human and Environmental Impact
+          <br/> through Social Enterprise</h1>
+      </div>
       </div>
 </div>
 <div className='socialimpact-container2'>
+  <div className='inner-social-impact-container2'>
+    <div className='content-section'>
+      <div className='content1 content-box'>
+<h1>733+</h1>
+<h2>People trained</h2>
+      </div>
+
+      <div className='content2 content-box'>
+<h1>733k</h1>
+<h2>careers launched</h2>
+      </div>
+
+      <div className='content3 content-box'>
+<h1>200+</h1>
+<h2>Projects finished</h2>
+      </div>
+
+      <div className='content4 content-box'>
+<h1>56</h1>
+<h2>Investors</h2>
+      </div>
+    </div>
+    <div className='image-content'>
   <div className='socialimpact-container2-image'>
     <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1681306013/lady-impact_rnc9jy.jpg'></img>
   </div>
@@ -35,26 +63,40 @@ function SocialImpact() {
     <h1>(Social) business for the win</h1>
     <p>With a young dynamic population that is eager to contribute to the global economy, East Africa has a lot to offer to the world. Now is the time to nurture sustainable, inclusive and scalable business. The rest will take care of itself.</p>
   </div>
+  </div>
+  </div>
 </div>
 <div className='mindset'>
-  <div className='mindset-content'>
+  <div className='inner-mindset'>
+    <div className='mindset-content'>
     <h1>A cooperative mindset</h1>
     <p>Elewa has one shared objective; To unlock the true potential of individuals, teams, and the community. We believe strongly in the power of sharing ideas and continuously strive to grow each other and ourselves. Internally, but also within the larger communities in which we are active.</p>
+  
+  </div>
   <div className='mindset-image'>
 <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1681308135/buffelo_i60xeu.jpg'></img>
   </div>
+  
   </div>
 </div>
+
+<div className='section3'>
 <div className='beyond'>
-<div className='beyond-content'>
+  <div className='inner-beyond'>
+     <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,q_90,w_400/v1675690301/elewa-group-website/Images/IMG_0764_twyo3k.png'></img>
+    <div className='beyond-content'>
+ 
 <h1>Beyond business</h1>
 <p>We channel a good amount of our resources into giving back.
 Through projects such as our coding school in Kakuma refugee camp, training teachers in pastoral Samburu and our open-source coding initiatives providing entry-level opportunities to hundreds of junior engineers
 These projects don't necessarily have an immediate financial return, but their impact can be tangibly measured in the long-term.</p>
 </div>
+  </div>
+</div>
 </div>
 <div className='knowledge'>
-<div className='knowledge-content'>
+  <div className='inner-knowledge'>
+    <div className='knowledge-content'>
 <h1>Open knowledge</h1>
 <p>What we learn, we share. Through community events, open knowledge repositories, regular teaching moments (everyone a teacher) and academic partnerships.
 We build for today, with a lens on tomorrow.</p>
@@ -62,26 +104,33 @@ We build for today, with a lens on tomorrow.</p>
 <div className='knowledge-image'>
 <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690302/elewa-group-website/Images/unnamed_1_m3dvll.png' alt='image'></img>
 </div>
+  </div>
+
 </div>
+<div className='impact'> 
 <div className='impact-projects'>
 <h1>Some of our impact projects</h1>
 </div>
+
 <div className='mission'>
 <h1>Become part of our mission
-and join elewa.</h1>
+ 
+and <span className='join'>join elewa.</span></h1>
 </div>
+</div>
+
   <div className='footer'>
-        <div className='footer-container'>
-            <div className='footer-container-1'>
-                <div className='Headquaters'>
+        <div className='footer-inner'>
+            <div className='footer-contacts'>
+                <div className='Headquartes footer-divs'>
                     <h2>Headquarters</h2>
                     <p>The Promenade, 19 General <br></br> Mathenge Rd <br></br> Nairobi, Kenya</p>
                 </div>
-                <div className='contacts-list'>
+                <div className='contacts footer-divs'>
                     <h2>Contacts</h2>
                     <p>T +254 78 92 27 755 <br></br> E info@elewa.ke</p>
                 </div>
-                <div className='navigation'>
+                <div className='navigation footer-divs'>
                     <h2>Navigation</h2>
                     <p>About us</p>
                     <p>Social<br></br>impact</p>
@@ -89,19 +138,20 @@ and join elewa.</h1>
                     <p>News</p>
                     <p>Contact</p>
                 </div>
-                <div  className='brands'>
+                <div  className='brands footer-divs'>
                     <h2>Brands</h2>
                     <p>Elewa</p>
                     <p>Italata</p>
                     <p>Venture labs</p>
                 </div>
-                <div className='privacy'>
+                <div className='privacy footer divs'>
                     <h2>Privacy</h2>
                     <p>Terms and<br></br>conditions</p>
                     <p>Cookie preferences</p>
                 </div>
             </div>
-            <div className='socials-tags'>
+            {/* social media platforms for the about page */}
+            <div className='footer-logo'>
                 <img src='https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg'></img>
                 <div className='social-media-icons'>
                     <div>
@@ -115,7 +165,7 @@ and join elewa.</h1>
                     </div>
                 </div>
             </div>
-                    </div>
+        </div>
       </div>
     </div>
     )
